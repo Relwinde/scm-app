23d8ec4824e896a3b88fd4537ecf8ff484e072c2
ouedgv@hotmail.fr <ouedgv@hotmail.fr>
2024-09-24 14:27:53 +0000

    menu reset

--- resources/views/layouts/components/footer.blade.php
+++ resources/views/layouts/components/footer.blade.php
@@ -3,7 +3,7 @@
 				 <div class="container">
 					 <div class="row align-items-center flex-row-reverse">
 						<div class="col-lg-12 col-sm-12  text-center ">
-							Copyright © 2023 <a href="javascript:void(0);">SCM LOGISTICS</a>. Designed with <span class="fa fa-heart text-danger"></span> by <a href="javascript:void(0);" class="text-primary"> Relwindér </a>  All rights reserved.
+							Copyright © 2023 <a href="javascript:void(0);">SCM LOGISTICS</a>. Designed with <span class="fa fa-heart text-danger"></span> by <a href="javascript:void(0);" class="text-primary"> Relwindé </a>  All rights reserved.
 						</div>
 					 </div>
 				 </div>
