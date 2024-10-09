f1f1915e2fd88dea0b1cd0bf024ab6829273dab2
ouedgv@hotmail.fr <ouedgv@hotmail.fr>
2024-10-09 14:27:52 +0000

    add forgot button for create

--- resources/views/livewire/outils/user.blade.php
+++ resources/views/livewire/outils/user.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.page-table', ['title'=>'Liste des utilisateurs'])
-
+@section('create-button')  
+    @can('Créer utlisateur')
+        <button wire:click="$dispatch('openModal', {component: '{{$create_modal}}'})" class="btn btn-primary mb-4"> {{$button_title}}</button>
+    @endcan
+@endsection
 @section('table')
     @include('partials.outils.users-table')
 @endsection
