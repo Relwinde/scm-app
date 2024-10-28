9a532f587d806284f72b13efce5d0a9c1e076603
ouedgv@hotmail.fr <ouedgv@hotmail.fr>
2024-10-28 17:58:16 +0000

    adding print bon print date

--- resources/views/prints/bon-de-caisse.blade.php
+++ resources/views/prints/bon-de-caisse.blade.php
@@ -1,9 +1,24 @@
 @php
+    use Carbon\Carbon;
+
     setlocale(LC_TIME, 'fr_FR.UTF-8');
+
+    $date = Carbon::now();
+
+    setlocale(LC_TIME, 'fr_FR.UTF-8');
+
+    $formattedDate = $date->translatedFormat('l d F Y \à H\h i\m s\s');
+
 @endphp
 
+<style>
+    td{
+        padding: 10px;
+    }
+</style>
+
 <center style="text-align: center;">
-        <p style="font-size: 14; margin-top:-50px;">
+        <p style="font-size: 14; margin-top:-150px;">
             Reçu de bon de caisse N° {{$bon->numero}}
         </p>
 </center>
@@ -11,12 +26,35 @@
 
 
 <div>
-    <p>Emétteur: <b>{{$bon->user->name}}</b></p>
-    <p>Dossier: <b>{{$bon->dossier->numero ?? $bon->transport->numero ?? "AUTRES"}}</b></p>
-    <p>Intitullé de la dépense: <b>{{$bon->depense}}</b></p>
-    <p>Montant: <b>{{number_format($bon->montant_definitif, 2, '.', ' ')}} CFA</b></p>
-    <p>Emis le: <b>{{ strftime("%e %B %Y", strtotime($bon->etapes()->where('etape_actuelle', 'RESPONSABLE')->first()->created_at)); }}</b></p>
-    <p>Payé le: <b>{{ strftime("%e %B %Y", strtotime($bon->etapes()->where('etape_actuelle', 'PAYE')->first()->created_at)); }}</b></p>
+    <table>
+        <tbody>
+            <tr>
+                <td>
+                    <p>Emétteur: <b>{{$bon->user->name}}</b></p>
+                </td>
+                <td>
+                    <p>Dossier: <b>{{$bon->dossier->numero ?? $bon->transport->numero ?? "AUTRES"}}</b></p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p>Intitullé de la dépense: <b>{{$bon->depense}}</b></p>
+                </td>
+                <td>
+                    <p>Montant: <b>{{number_format($bon->montant_definitif, 2, '.', ' ')}} CFA</b></p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p>Emis le: <b>{{ strftime("%e %B %Y", strtotime($bon->etapes()->where('etape_actuelle', 'RESPONSABLE')->first()->created_at)); }}</b></p>
+                </td>
+                <td>
+                    <p>Payé le: <b>{{ strftime("%e %B %Y", strtotime($bon->etapes()->where('etape_actuelle', 'PAYE')->first()->created_at)); }}</b></p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
 
 </div>
 
@@ -41,5 +79,11 @@
             </tr>
         </tbody>
     </table>
+
+    <center style="text-align: center;">
+        <p style="font-size: 10; margin-top:20px;">
+            Reçu imprimé le {{$formattedDate}}
+        </p>
+</center>
 </div>
 
