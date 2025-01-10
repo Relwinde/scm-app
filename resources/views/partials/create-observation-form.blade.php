c2098ce9e91bfdc35124f8126aa1fdb93f3760ea
ouedgv@hotmail.fr <ouedgv@hotmail.fr>
2025-01-10 12:33:05 +0000

    tryging to fix observations author

--- resources/views/partials/create-observation-form.blade.php
+++ resources/views/partials/create-observation-form.blade.php
@@ -1,12 +1,12 @@
-<form wire:submit.prevent="create" >
-    <div class="card form-input-elements">
-        <div class="card-header justify-content-between">
-            <h3 class="mb-0 card-title"><b>{{$title}} {{$dossier->numero}}</b></h3>
-            <div class="btn-group" role="group" aria-label="Basic example">
-                <a wire:click="$dispatch('openModal', {component: 'modals.view-dossier', arguments: { dossier : {{ $dossier->id }} }})" href="javascript:void(0);" class="btn btn-outline-primary">Retour au dossier</a>
-                <a wire:click="$dispatch('openModal', {component: 'modals.dossier.view-observations', arguments: { dossier : {{ $dossier->id }} }})" href="javascript:void(0);" class="btn btn-outline-primary">Retour aux commentaires</a>
-            </div>
+<div class="card form-input-elements">
+    <div class="card-header justify-content-between">
+        <h3 class="mb-0 card-title"><b>{{$title}} {{$dossier->numero}}</b></h3>
+        <div class="btn-group" role="group" aria-label="Basic example">
+            <a wire:click="$dispatch('openModal', {component: 'modals.view-dossier', arguments: { dossier : {{ $dossier->id }} }})" href="javascript:void(0);" class="btn btn-outline-primary">Retour au dossier</a>
+            <a wire:click="$dispatch('openModal', {component: 'modals.dossier.view-observations', arguments: { dossier : {{ $dossier->id }} }})" href="javascript:void(0);" class="btn btn-outline-primary">Retour aux commentaires</a>
         </div>
+    </div>
+    <form wire:submit.prevent="create" >
         <div class="card-body">
             <div class="row">
                 <div class="col-md-12 ">
@@ -19,9 +19,9 @@
         </div>
         <div class="card-footer">
             <div class="btn-list">
-                <button href="javascript:void(0);" class="btn btn-primary">Enregistrer</button>
+                <button class="btn btn-primary">Enregistrer</button>
                 <a href="javascript:void(0);" wire:click="$dispatch('closeModal')" class="btn btn-danger">Annuler</a>
             </div>
         </div>
+    </form>
     </div>
-</form>
